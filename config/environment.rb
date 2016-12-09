df9c5d2d80b8ad8120e716c58f72c7e7f6ca7695
Hachiko001 <nguyenvanthanhtu@gmail.com>
2016-12-09 15:07:58 +0700

    Setup 1
    Add gem file for mailer postgre and some another stuff.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.default :content_type => "text/html"
+
+# SMTP settings for gmail
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'pkmgoclone1@gmail.com',
+ :password             => 'shaolin25',
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
