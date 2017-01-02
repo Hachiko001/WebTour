bbf1f6f647c48e434bf830f248d488d0466d5c50
phuquoc <votruongtoan123@gmail.com>
2017-01-02 11:08:19 +0700

    Image Uploader

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,2 +1,11 @@
 class Guide < ApplicationRecord
+mount_uploader :image, ImageUploader # Tells rails to use this uploader for this model.
+
+def image_url
+    if image.present?
+      image
+    else
+    "#"
+    end
+  end
 end
