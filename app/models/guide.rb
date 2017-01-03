02e79072210bc9ff26bb7d11b5747a43906b1b17
PinKu0702 <hungduy0702@gmail.com>
2017-01-03 19:21:35 +0700

    Search
    Search by name and language

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,2 +1,11 @@
 class Guide < ApplicationRecord
+
+
+def self.search_name(key)
+   where('name like ?', "%#{key}%")
+end
+def self.search_language(key)
+   where('language like ?', "%#{key}%")
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227043545) do
+ActiveRecord::Schema.define(version: 20161227045120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "guide_id"
+    t.string   "title"
+    t.string   "content"
+    t.float    "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guides", force: :cascade do |t|
     t.string   "name"
