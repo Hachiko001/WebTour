693b37b69a95bba81decf7852cd8370fad89f477
PinKu0702 <hungduy0702@gmail.com>
2017-01-03 19:31:48 +0700

    search

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,5 +1,4 @@
 class Guide < ApplicationRecord
-<<<<<<< HEAD
 
 
 def self.search_name(key)
@@ -8,16 +7,4 @@ end
 def self.search_language(key)
    where('language like ?', "%#{key}%")
 end
-
-=======
-mount_uploader :image, ImageUploader # Tells rails to use this uploader for this model.
-
-def image_url
-    if image.present?
-      image
-    else
-    "#"
-    end
-  end
->>>>>>> origin/master
 end
