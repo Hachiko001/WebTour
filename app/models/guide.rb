b1625fce5697fa436629fa6f10e18efcf4e6363a
phuquoc <votruongtoan123@gmail.com>
2017-01-04 14:31:20 +0700

    index guide

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,10 +1,20 @@
 class Guide < ApplicationRecord
 
-
+mount_uploader :image, ImageUploader # Tells rails to use this uploader for this model.
 def self.search_name(key)
    where('name like ?', "%#{key}%")
 end
 def self.search_language(key)
    where('language like ?', "%#{key}%")
 end
+
+def image_url
+    if image.present?
+      image
+    else
+      "http://loremflickr.com/320/240/#{CGI.escape name}"
+    # onchange="readURL(this)
+    "#"
+    end
+  end
 end
